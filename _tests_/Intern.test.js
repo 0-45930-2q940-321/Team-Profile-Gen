ce23c546438ab10feca6554b916f869e5163a72d
906284 <michaelcounseller@gmail.com>
2022-01-07 14:51:42 -0600

    corrected the spelling of, createdIntern variable

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,14 +2,14 @@ const Intern = require('../lib/Intern');
 
 describe('Collect the Interns school name and create an Intern role', () => {
     it('should create an Intern', () => {
-        const createedIntern = new Intern();
+        const createdIntern = new Intern();
     
-        expect(createedIntern.setJobRole()).toMatch('Intern');
+        expect(createdIntern.setJobRole()).toMatch('Intern');
     });
     
     it('should get a school name', () => {
-        const createedIntern = new Intern('Testing', '3', 'email3@email.com', 'Somewhere School');
+        const createdIntern = new Intern('Testing', '3', 'email3@email.com', 'Somewhere School');
     
-        expect(createedIntern.school).toMatch('Somewhere School');
+        expect(createdIntern.school).toMatch('Somewhere School');
     });
 });
