38e36f33ba65f415d88862002c68ae0e5601f72d
906284 <michaelcounseller@gmail.com>
2022-01-07 14:57:28 -0600

    testing origin change

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Employee = require ('../lib/Employee.js');
+
+const question = [
+    {
+        type: 'text',
+        name: 'name',
+        message: 'To begin s'
+    }
+]
