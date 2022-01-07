ba565b99b15e2ad4d22d6dbfb6ea9d95750eb719
906284 <michaelcounseller@gmail.com>
2022-01-07 15:42:47 -0600

    added base questions for the inquirer

--- index.js
+++ index.js
@@ -6,6 +6,39 @@ const question = [
     {
         type: 'text',
         name: 'name',
-        message: 'To begin s'
+        message: 'To begin enter the Employee\'s name.'
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What is the Employee\'s role for the job?',
+        choices: ['Engineer', 'Intern', 'Manager']
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What Id Number would you like to give to the Employee?',
+
+        validate: idInput => {
+            if(!isNan(idInput)) {
+                console.log('Enter a number. Letters will not be Accepted')
+                return false;
+
+            } else if (idInput.length < 6) {
+                console.log('Please keep the number count below or equal to 6')
+                return false;
+
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type:'input',
+        name: 'email',
+        message: 'Enter the Employee\'s email'
     }
-]
+];
+
+
+
