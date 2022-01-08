df26d45cf399ab68db69f5b9fc71752f80aa4736
906284 <michaelcounseller@gmail.com>
2022-01-07 19:24:27 -0600

    working CLI with node. Proper inquirer prompts for each employee role. storing all the answers into a variable and pushing the variable data into an array

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const Employee = require ('../lib/Employee.js');
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
 
-const question = [
+const questions = [
     {
         type: 'text',
         name: 'name',
@@ -20,12 +21,9 @@ const question = [
         message: 'What Id Number would you like to give to the Employee?',
 
         validate: idInput => {
-            if(!isNan(idInput)) {
-                console.log('Enter a number. Letters will not be Accepted')
-                return false;
 
-            } else if (idInput.length < 6) {
-                console.log('Please keep the number count below or equal to 6')
+            if (idInput.length > 6) {
+                console.log(' <<ERROR>> PLEASE KEEP THE NUMBER COUNT BELOW OR EQUAL TO 6')
                 return false;
 
             } else {
@@ -34,11 +32,123 @@ const question = [
         }
     },
     {
-        type:'input',
+        type: 'input',
         name: 'email',
         message: 'Enter the Employee\'s email'
     }
 ];
 
+createEmployee = () => {
 
+    let employeeArray = [];
 
+    const askQuestions = () => {
+
+        inquirer.prompt(questions)
+            .then(data => {
+
+                if (data.role === 'Engineer') {
+                    let createdEngineer;
+
+                    createdEngineer = new Engineer(data.name, data.id, data.email, data.role);
+
+                    inquirer
+                        .prompt([
+                            {
+                                type: 'text',
+                                name: 'github',
+                                message: 'What is your Github user?'
+                            },
+                            {
+                                type: 'confirm',
+                                name: 'addEmployee',
+                                message: 'Would you like to add another Employee?',
+                                default: false,
+                            }
+                        ])
+                        .then(data => {
+
+                            employeeArray.push(createdEngineer);
+                            console.table(employeeArray);
+
+                            if (data.addEmployee) {
+                                askQuestions();
+
+                            } else {
+                                return false;
+                            };
+                        })
+                } else if (data.role === 'Intern') {
+                    let createdIntern;
+
+                    createdIntern = new Intern(data.name, data.id, data.email, data.role);
+                    inquirer
+                        .prompt([
+                            {
+                                type: 'text',
+                                name: 'school',
+                                message: 'What school is the Intern going to?'
+                            },
+                            {
+                                type: 'confirm',
+                                name: 'addEmployee',
+                                message: 'Would you like to add another Employee?',
+                                default: false,
+                            }
+                        ])
+                        .then(data => {
+
+                            employeeArray.push(createdIntern);
+                            console.table(employeeArray);
+
+                            if (data.addEmployee) {
+                                askQuestions();
+
+                            } else {
+                                return false;
+                            };
+                        })
+                } else if (data.role === 'Manager') {
+                    let createdManager;
+
+                    createdManager = new Manager(data.name, data.id, data.email, data.role)
+                    inquirer
+                        .prompt([
+                            {
+                                type: 'text',
+                                name: 'officeNum',
+                                message: 'What is the Manager\'s office number?'
+                            },
+                            {
+                                type: 'confirm',
+                                name: 'addEmployee',
+                                message: 'Would you like to add another Employee?',
+                                default: false,
+                            }
+                        ])
+                        .then(data => {
+
+                            employeeArray.push(createdManager);
+                            console.table(employeeArray);
+
+                            if (data.addEmployee) {
+                                askQuestions();
+
+                            } else {
+                                return false;
+                            };
+                            
+                        }) ;
+
+                        console.table(employeeArray);
+                        
+                } else {
+                    return false;
+                };
+                console.table(employeeArray);
+            });
+    }
+    askQuestions();
+};
+
+createEmployee();
